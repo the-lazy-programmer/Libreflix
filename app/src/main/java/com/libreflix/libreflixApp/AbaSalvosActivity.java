e95038b92565ec67ec90b93b9ac4f0ea5e7cf01a
joao <jmarcelopq@gmail.com>
2024-11-29 17:47:17 -0300

    Ajeitei os botões de AbaSalvosActivity

--- app/src/main/java/com/libreflix/libreflixApp/AbaSalvosActivity.java
+++ app/src/main/java/com/libreflix/libreflixApp/AbaSalvosActivity.java
@@ -1,5 +1,6 @@
 package com.libreflix.libreflixApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,10 +24,10 @@ public class AbaSalvosActivity extends AppCompatActivity {
         ImageView imageView21 = findViewById(R.id.imageView21);
         ImageView imageView22 = findViewById(R.id.imageView22);
 
-        Button salvos3 = findViewById(R.id.salvos3);
-        Button salvos4 = findViewById(R.id.salvos4);
-        Button salvos5 = findViewById(R.id.salvos5);
-        Button salvos6 = findViewById(R.id.salvos6);
+        Button salvos3 = findViewById(R.id.homeSalvos);
+        Button salvos4 = findViewById(R.id.salvosSalvos);
+        Button salvos5 = findViewById(R.id.salvosBuscar);
+        Button salvos6 = findViewById(R.id.salvosEmAlta);
         Button salvos7 = findViewById(R.id.salvos7);
         Button salvos8 = findViewById(R.id.salvos8);
         Button salvos9 = findViewById(R.id.salvos9);
@@ -35,48 +36,72 @@ public class AbaSalvosActivity extends AppCompatActivity {
         salvos3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, HomeActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
 
         salvos4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, AbaSalvosActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
 
         salvos5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, AbaBuscarActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
 
         salvos6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, AbaEmAltaActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
 
         salvos7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, TelaFilmeActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
 
         salvos8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, TelaFilmeActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
 
         salvos9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, TelaFilmeActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
 
         salvos10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(AbaSalvosActivity.this, TelaFilmeActivity.class);
+                intent.putExtra("perfilUsuario", perfilUsuario); // Pass as Serializable
+                startActivity(intent);
             }
         });
     }
