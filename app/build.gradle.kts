61ab419a3127242b37328b37a72f29f8986668d9
lil pedro <pedroantoniomergulhao@gmail.com>
2024-11-29 00:17:54 -0300

    perfilactivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.filament.android)
     implementation(libs.room.runtime)
+    implementation(libs.room.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/libreflix/libreflixApp/AbaSalvosActivity.java
+++ app/src/main/java/com/libreflix/libreflixApp/AbaSalvosActivity.java
@@ -1,5 +1,84 @@
 package com.libreflix.libreflixApp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SalvosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abasalvos);
+
+        ImageView imageView12 = findViewById(R.id.imageView12);
+        ImageView imageView19 = findViewById(R.id.imageView19);
+        ImageView imageView21 = findViewById(R.id.imageView21);
+        ImageView imageView22 = findViewById(R.id.imageView22);
+
+        Button salvos3 = findViewById(R.id.salvos3);
+        Button salvos4 = findViewById(R.id.salvos4);
+        Button salvos5 = findViewById(R.id.salvos5);
+        Button salvos6 = findViewById(R.id.salvos6);
+        Button salvos7 = findViewById(R.id.salvos7);
+        Button salvos8 = findViewById(R.id.salvos8);
+        Button salvos9 = findViewById(R.id.salvos9);
+        Button salvos10 = findViewById(R.id.salvos10);
+
+        salvos3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        salvos4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        salvos5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        salvos6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        salvos7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        salvos8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        salvos9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        salvos10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+}
+
+
 public class AbaSalvosActivity {
 
     private  String filmesSalvos[];
